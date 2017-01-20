6a46846720d4253d34bcb9d956a4acf7efb75c12
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-01-20 13:48:13 -0500

    cp3: auto add room names

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,36 @@
 (function() {
     function Room($firebaseArray) {
         var Room = {};
+        var currentRoom = null;
 
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
 
         Room.all = rooms;
+
+        Room.add = function(room) {
+          rooms.$add(room);
+        }
+
+        // function writeUserData(userId, name, email, imageUrl) {
+        //   firebase.database().ref('users/' + userId).set({
+        //     username: name,
+        //     email: email,
+        //     profile_picture : imageUrl
+        //   });
+        // }
+
+        // var list = $firebaseArray(ref);
+        // list.$add({ foo: "bar" }).then(function(ref) {
+        //   var id = ref.key;
+        //   console.log("added record with id " + id);
+        //   list.$indexFor(id); // returns location in the array
+        // });
+
+        Room.setRoom = function(room) {
+          this.currentRoom = room;
+          console.log(room);
+        }
 
         return Room;
     }
--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,6 +1,12 @@
 (function() {
     function LandingCtrl(Room) {
         this.rooms = Room.all;
+
+        this.addRoom = function(room) {
+            console.log(room);
+            Room.add(room);
+            this.newRoom.name = "";
+        }
     }
 
     angular
